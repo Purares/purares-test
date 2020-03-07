f25c744927d7f3b9e38d45438682e7423403f57d
Nicolas Marcos De Lucca <ndelucca@garbarino.com.ar>
2020-03-06 22:01:15 -0300

    Nico ACTUALIZACION5

---
+++ modelo/formulariosMDL.php
@@ -0,0 +1,356 @@
+<?php
+
+require_once"modelo/conexion.php";
+
+$conexion=Conexion::conectarBD();
+
+class ModeloFormularios{
+
+
+	#------------------------- Lista  DEPOSITOS -------------------------#
+
+
+	static public function mdlListaDeposito(){
+
+		$stmt=conexion::conectarBD()->prepare("SELECT id_deposito, nombre FROM depositos_n");
+		$stmt -> execute();
+		return $stmt -> fetchAll(); #fetchAll devuelvo todos los registros
+		$stmt -> close(); #cierra la conexion
+		$stmt =null; 
+
+	}
+
+
+	#------------------------- Lista UDM -------------------------#
+
+
+	static public function mdlListaUDM(){
+
+		$stmt=conexion::conectarBD()->prepare("SELECT id_udm, nombre FROM udm_n");
+		$stmt -> execute();
+		return $stmt -> fetchAll(); #fetchAll devuelvo todos los registros
+		$stmt -> close(); #cierra la conexion
+		$stmt =null; 
+	}
+
+#------------------------- Lista CUENTAS -------------------------#
+
+
+	static public function mdlListaCuentas(){
+
+		$stmt=conexion::conectarBD()->prepare("SELECT id_cuenta, nombre FROM cuentas_n");
+		$stmt -> execute();
+		return $stmt -> fetchAll(); #fetchAll devuelvo todos los registros
+		$stmt -> close(); #cierra la conexion
+		$stmt =null; 
+	}
+
+
+	#------------------------- Stock Insumos -------------------------#
+
+	static public function mdlStockInsumo(){
+ 
+		$stmt=conexion::conectarBD()->prepare("select * from v_stockinsumos;");
+		$stmt -> execute();
+		return $stmt -> fetchAll(); #fetchAll devuelvo todos los registros
+		$stmt -> close(); #cierra la conexion
+		$stmt =null; 
+
+	}
+
+
+	#------------------------- AGREGAR INSUMO -------------------------#
+
+	static public function mdlAgregarInsumo($datos){
+
+		$stmt=conexion::conectarBD()->prepare("call ins_AgregarInsumo( :nombreInsumo,:id_udn,:id_deposito,:alertaQmim );");
+		
+		$stmt -> bindparam (":nombreInsumo",$datos['nombreInsumo_'],PDO::PARAM_STR);
+		$stmt -> bindparam (":idDeposito",$datos['idDeposito_'],PDO::PARAM_INT);
+		$stmt -> bindparam (":idUm",$datos['idUm_'],PDO::PARAM_INT);
+		$stmt -> bindparam (":alertaQmin",$datos ['alertaQmin_'],PDO::PARAM_STR);
+
+
+		if ($stmt -> execute()){
+			return "OK"; #si se ejecutó correctamente le envío un OK
+
+		}else{
+			print_r(conexion::conectarBD());#Si se ejecutó con error le envío el error}
+		}
+		
+		$stmt -> close(); #cierra la conexion
+		$stmt =null;
+	}
+
+	#------------------------- ACTUALIZAR STOCK DE INSUMO -------------------------#
+
+	static public function mdlActualizarInsumo($datos){
+
+		$stmt=conexion::conectarBD()->prepare("call ins_AgreagarMovInsumo( :id_insumo,:cantidad,:id_cuenta,:comentario,id_usuario);");
+		
+		$stmt -> bindparam (":id_insumo",$datos['idInsumo_'],PDO::PARAM_INT);
+		$stmt -> bindparam (":cantidad",$datos['cantidad_'],PDO::PARAM_STR); #[TO DO] Cambiar a decimal
+		$stmt -> bindparam (":id_cuenta",$datos['idCuenta_'],PDO::PARAM_INT);
+		$stmt -> bindparam (":comentario",$datos['comentario_'],PDO::PARAM_STR); 
+		$stmt -> bindparam (":id_usuario",$datos['idUsuario_'],PDO::PARAM_INT);
+
+		if ($stmt -> execute()){
+			return "OK"; #si se ejecutó correctamente le envío un OK
+
+		}else{
+			print_r(conexion::conectarBD());#Si se ejecutó con error le envío el error}
+		}
+		
+		$stmt -> close(); #cierra la conexion
+		$stmt =null;
+	}
+
+
+#------------------------- Insumos por Depositos -------------------------#
+
+
+	static public function mdlInsumosDeposito($id_deposito){
+
+		$stmt=conexion::conectarBD()->prepare("	Select id_insumo, nombre from insumos_n where id_deposito=$id_deposito order by nombre;");
+		$stmt -> execute();
+		return $stmt -> fetchAll(); #fetchAll devuelvo todos los registros
+		$stmt -> close(); #cierra la conexion
+		$stmt =null; 
+	}
+
+#------------------------- UDM de Insumo  -------------------------#
+
+
+	static public function mdlUdmInsumo($id_insumo){
+
+		$stmt=conexion::conectarBD()->prepare("select * from v_udminsumo where id_insumo=$id_insumo;");
+		$stmt -> execute();
+		return $stmt -> fetchAll(); #fetchAll devuelvo todos los registros
+		$stmt -> close(); #cierra la conexion
+		$stmt =null; 
+	}
+
+
+#------------------------- Lista RECETAS -------------------------#
+
+
+	static public function mdlListaRecetas(){
+
+		$stmt=conexion::conectarBD()->prepare("SELECT * FROM recetas_n");
+		$stmt -> execute();
+		return $stmt -> fetchAll(); #fetchAll devuelvo todos los registros
+		$stmt -> close(); #cierra la conexion
+		$stmt =null; 
+	}
+
+
+#------------------------- Detalle de RECETA -------------------------#
+
+
+	static public function mdlDetalleReceta($id_receta){
+
+		$stmt=conexion::conectarBD()->prepare("SELECT * FROM recetas_n where id_receta=$id_receta");
+		$stmt -> execute();
+		return $stmt -> fetchAll(); #fetchAll devuelvo todos los registros
+		$stmt -> close(); #cierra la conexion
+		$stmt =null; 
+	}
+
+
+#------------------------- Insumos de RECETA -------------------------#
+
+
+	static public function mdlInsumosReceta($id_receta){
+
+		$stmt=conexion::conectarBD()->prepare("call v_InsumosReceta($id_receta);");
+		$stmt -> execute();
+		return $stmt -> fetchAll(); #fetchAll devuelvo todos los registros
+		$stmt -> close(); #cierra la conexion
+		$stmt =null; 
+	}
+
+
+
+#------------------------- Inactivar RECETA -------------------------#
+
+	static public function mdlDesactivarReceta($id_receta){
+
+		$stmt=conexion::conectarBD()->prepare("call act_InactivarReceta($id_receta);");
+		
+		if ($stmt -> execute()){
+			return "ok";
+		}else{ 
+			print_r(conexion::conectarBD()->errorInfo());
+		}
+		
+		$stmt -> close(); #cierra la conexion
+		$stmt =null;  
+	}
+
+#------------------------- Activar RECETA -------------------------#
+
+	static public function mdlActivarReceta($id_receta){
+
+		$stmt=conexion::conectarBD()->prepare("call act_ActivarReceta($id_receta);");
+		
+		if ($stmt -> execute()){
+			return "ok";
+		}else{ 
+			print_r(conexion::conectarBD()->errorInfo());
+		}
+		
+		$stmt -> close(); #cierra la conexion
+		$stmt =null;  
+	}
+
+
+#------------------------- Crear RECETA -------------------------#
+
+	static public function mdlCrearReceta($datos){
+
+		$stmt=conexion::conectarBD()->prepare("call ins_AgreagarReceta( :nombre,:merma,:diaspord,:diasvenc,:largouni,:pesouni,:porcentcarne,:descripcion);");
+		
+		$stmt -> bindparam (":nombre",$datos['nombre_'],PDO::PARAM_STR);
+		$stmt -> bindparam (":merma",$datos['merma_'],PDO::PARAM_STR);
+		$stmt -> bindparam (":diaspord",$datos['diasprod_'],PDO::PARAM_STR);
+		$stmt -> bindparam (":diasvenc",$datos['diasvenc_'],PDO::PARAM_STR); 
+		$stmt -> bindparam (":largouni",$datos['largouni_'],PDO::PARAM_STR);
+		$stmt -> bindparam (":pesouni",$datos['pesouni_'],PDO::PARAM_STR);
+		$stmt -> bindparam (":porcentcarne",$datos['porcentcarne_'],PDO::PARAM_STR);
+		$stmt -> bindparam (":descripcion",$datos['descripcion_'],PDO::PARAM_STR);
+
+		if ($stmt -> execute()){
+
+			return "ok";
+		}else{ 
+			print_r(conexion::conectarBD()->errorInfo());
+		}
+
+		$stmt -> close(); #cierra la conexion
+		$stmt =null; 
+	}
+
+
+#------------------------- Crear RECETA -------------------------#
+
+	static public function mdlAltaInsumosReceta($idReceta_nueva,$datos3){
+
+		$stmt=conexion::conectarBD()->prepare("call ins_AgregarInsumosXReceta($idReceta_nueva,:id_insumo,:cantidad);");
+		
+		$stmt -> bindparam (":id_insumo",$datos['id_insumo_'],PDO::PARAM_INT);
+		$stmt -> bindparam (":cantidad",$datos['cantidad_insumo_'],PDO::PARAM_STR);
+
+		if ($stmt -> execute()){
+			return "ok";
+		}else{ 
+			print_r(conexion::conectarBD()->errorInfo());
+		}
+
+		$stmt -> close(); #cierra la conexion
+		$stmt =null; 
+	}
+
+
+#------------------------- Lista CARNES -------------------------#
+
+
+	static public function mdlListaCarnes(){
+
+		$stmt=conexion::conectarBD()->prepare("SELECT * FROM carnes_n");
+		$stmt -> execute();
+		return $stmt -> fetchAll(); #fetchAll devuelvo todos los registros
+		$stmt -> close(); #cierra la conexion
+		$stmt =null; 
+	}
+
+
+#------------------------- Stock CARNES------------------------#
+
+	static public function mdlStockCarnes(){
+ 
+		$stmt=conexion::conectarBD()->prepare("SELECT * FROM v_stockinsumos;");
+		$stmt -> execute();
+		return $stmt -> fetchAll(); #fetchAll devuelvo todos los registros
+		$stmt -> close(); #cierra la conexion
+		$stmt =null; 
+
+	}
+
+
+	#------------------------- AGREGAR CARNE -------------------------#
+
+	static public function mdlAgregarCarne($datos){
+
+		$stmt=conexion::conectarBD()->prepare("call `purares-test`.ins_AgregarCarne(:nombreCarne, :idUDM, :alertaQmin);");
+		
+		$stmt -> bindparam (":nombreCarne",$datos['nombreCarne_'],PDO::PARAM_STR);
+		$stmt -> bindparam (":idUDM",$datos['idUDM_'],PDO::PARAM_INT);
+		$stmt -> bindparam (":alertaQmin",$datos['alertaQmin_'],PDO::PARAM_STR);
+
+
+		if ($stmt -> execute()){
+			return "OK"; #si se ejecutó correctamente le envío un OK
+
+		}else{
+			print_r(conexion::conectarBD());#Si se ejecutó con error le envío el error}
+		}
+		
+		$stmt -> close(); #cierra la conexion
+		$stmt =null;
+	}
+
+#------------------------- Registro de DESBASTE------------------------#
+
+	static public function mdlVerRegistroDesbaste(){
+ 
+		$stmt=conexion::conectarBD()->prepare("SELECT * FROM v_desbastes;");
+		$stmt -> execute();
+		return $stmt -> fetchAll(); #fetchAll devuelvo todos los registros
+		$stmt -> close(); #cierra la conexion
+		$stmt =null; 
+
+	}
+
+
+
+
+
+
+
+
+/*
+
+#------------------------- ELIMINAR SE UTILIZA PARA REALIZAR PRUEBAS! -------------------------#
+
+	static public function mdlprueba(){
+
+		$stmt=Conexion::conectarBD()->prepare("call ins_AgregarReceta('prueba202', 0.23, 12, 43, 60, 300, 0.9, 'creando una nueva receta ');
+");
+
+		if ($stmt -> execute()){
+
+			#$con = conexion::conectarBD()->mysqli_connect();
+
+		# [TO DO] NO FUNCIONA NINGUNA DE LAS OPCIONES!!!!!
+			#$id_insertado= mysqli_insert_id($con);
+			$id_insertado= Conexion::conectarBD()->lastInsertId();
+			#$id_insertado=conexion::conectarBD()->prepare("SELECT LAST_INSERT_ID();");
+			#$id_insertado=1;
+
+			return "$id_insertado";
+
+
+		}else{ 
+			print_r(Conexion::conectarBD()->errorInfo());
+		}
+
+		$stmt -> close(); #cierra la conexion
+		$stmt =null; 
+	}
+
+*/
+
+
+} #Cierra la clase
+
+?>
