8b8186b32e4317f456fb03619f8cf1d8610ea8a5
mesuppes <mesuppes22@gmail.com>
2020-03-19 00:38:18 -0300

    pantalla agregar compra

--- vista/plantilla.php
+++ vista/plantilla.php
@@ -110,7 +110,7 @@
      <li class="nav-item dropdown">
     <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Configuracion</a>
     <div class="dropdown-menu">
-      <a class="dropdown-item" href="index.php?pagina=agregarCompra">Agregar compra</a>
+      <a class="dropdown-item" href="index.php?pagina=agregarCompra&tipoProveedor_NuevaCompra=%27Insumos%27">Agregar compra</a>
     </div>
 		</li>
 
