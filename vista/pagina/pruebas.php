5848f26344fdeb0445d591973cb23423d043c851
Nicolas Marcos De Lucca <ndelucca@garbarino.com.ar>
2020-03-27 19:01:29 -0300

    Isset finOP

--- vista/pagina/pruebas.php
+++ vista/pagina/pruebas.php
@@ -5,11 +5,16 @@
 <?php
 
 $id_receta=117;
+$cantidad_carne=[100,200,100];
+
 
 $detalleReceta=ModeloFormularios::mdlDetalleReceta($id_receta);
 
 $porcentCarne=$detalleReceta [0]['porcent_carne'];
 
+$cantidadTotalCarne=array_sum($cantidad_carne);
+
+var_dump($cantidadTotalCarne);
 var_dump($porcentCarne);		
 var_dump($detalleReceta);		
 
